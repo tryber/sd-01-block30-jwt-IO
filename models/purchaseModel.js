3df12e3913ffc9527ab5fb4c8fdce8f32d8e1641
Gabriel Dalseco dos Santos <gabriel_santos96@outlook.com>
2020-05-13 18:54:03 -0300

    code climate

--- models/purchaseModel.js
+++ models/purchaseModel.js
@@ -1,82 +1,82 @@
-const fs = require('fs').promises;
-const path = require('path');
+// const fs = require('fs').promises;
+// const path = require('path');
 
-class Purchase {
-  constructor(userID, productId, quantity) {
-    this.id = null;
-    this.userID = userID;
-    this.productId = productId;
-    this.quantity = quantity;
-  }
+// class Purchase {
+//   constructor(userID, productId, quantity) {
+//     this.id = null;
+//     this.userID = userID;
+//     this.productId = productId;
+//     this.quantity = quantity;
+//   }
 
-  async getAll(id) {
-    const rawData = await fs.readFile(path.resolve(__dirname, '..', 'data', 'purchases.json'), 'utf8');
-    const purchases = JSON.parse(rawData)
-      .filter((purchase) => purchase.id === parseInt(id))
+//   async getAll(id) {
+//     const rawData = await fs.readFile(path.resolve(__dirname, '..', 'data', 'purchases.json'), 'utf8');
+//     const purchases = JSON.parse(rawData)
+//       .filter((purchase) => purchase.id === parseInt(id))
 
-    return purchases;
-  }
+//     return purchases;
+//   }
 
-  async getById(id) {
-    const rawData = await fs.readFile(path.resolve(__dirname, '..', 'data', 'purchases.json'), 'utf8');
-    const purchase = JSON.parse(rawData)
-      .find((purchase) => purchase.id === parseInt(id));
+//   async getById(id) {
+//     const rawData = await fs.readFile(path.resolve(__dirname, '..', 'data', 'purchases.json'), 'utf8');
+//     const purchase = JSON.parse(rawData)
+//       .find((purchase) => purchase.id === parseInt(id));
 
-    return purchase;
-  }
+//     return purchase;
+//   }
 
-  async add() {
-    const rawData = await fs.readFile(path.resolve(__dirname, '..', 'data', 'products.json'), 'utf8');
-    const products = JSON.parse(rawData);
+//   async add() {
+//     const rawData = await fs.readFile(path.resolve(__dirname, '..', 'data', 'products.json'), 'utf8');
+//     const products = JSON.parse(rawData);
 
-    this.id = products[products.length - 1].id + 1
-    products.push(this);
+//     this.id = products[products.length - 1].id + 1
+//     products.push(this);
 
-    await fs.writeFile(path.resolve(__dirname, '..', 'data', 'products.json'),
-      JSON.stringify(products), 'utf8', (err) => {
-        if (err) throw err;
-        console.log('Ocorreu algum erro!');
-      });
+//     await fs.writeFile(path.resolve(__dirname, '..', 'data', 'products.json'),
+//       JSON.stringify(products), 'utf8', (err) => {
+//         if (err) throw err;
+//         console.log('Ocorreu algum erro!');
+//       });
 
-    return this;
-  }
+//     return this;
+//   }
 
-  async delete(id) {
-    const rawData = await fs.readFile(path.resolve(__dirname, '..', 'data', 'products.json'), 'utf8');
-    const products = JSON.parse(rawData).filter(product => product.id !== parseInt(id));
+//   async delete(id) {
+//     const rawData = await fs.readFile(path.resolve(__dirname, '..', 'data', 'products.json'), 'utf8');
+//     const products = JSON.parse(rawData).filter(product => product.id !== parseInt(id));
 
-    await fs.writeFile(path.resolve(__dirname, '..', 'data', 'products.json'),
-      JSON.stringify(products), 'utf8', (err) => {
-        if (err) throw err;
-        console.log('Ocorreu algum erro!');
-      });
+//     await fs.writeFile(path.resolve(__dirname, '..', 'data', 'products.json'),
+//       JSON.stringify(products), 'utf8', (err) => {
+//         if (err) throw err;
+//         console.log('Ocorreu algum erro!');
+//       });
 
-    return products;
-  }
+//     return products;
+//   }
 
-  async addOrUpdate(id) {
-    const rawData = await fs.readFile(path.resolve(__dirname, '..', 'data', 'products.json'), 'utf8');
-    const products = JSON.parse(rawData);
+//   async addOrUpdate(id) {
+//     const rawData = await fs.readFile(path.resolve(__dirname, '..', 'data', 'products.json'), 'utf8');
+//     const products = JSON.parse(rawData);
 
-    const product = products.find(product => product.id === parseInt(id));
+//     const product = products.find(product => product.id === parseInt(id));
 
-    if (product) {
-      product.name = this.name;
-      product.description = this.description;
-      product.price = this.price;
-    } else {
-      this.id = products[products.length - 1].id + 1;
-      products.push(this);
-    }
+//     if (product) {
+//       product.name = this.name;
+//       product.description = this.description;
+//       product.price = this.price;
+//     } else {
+//       this.id = products[products.length - 1].id + 1;
+//       products.push(this);
+//     }
 
-    await fs.writeFile(path.resolve(__dirname, '..', 'data', 'products.json'),
-      JSON.stringify(products), 'utf8', (err) => {
-        if (err) throw err;
-        console.log('Ocorreu algum erro!');
-      });
+//     await fs.writeFile(path.resolve(__dirname, '..', 'data', 'products.json'),
+//       JSON.stringify(products), 'utf8', (err) => {
+//         if (err) throw err;
+//         console.log('Ocorreu algum erro!');
+//       });
 
-    return products;
-  }
-}
+//     return products;
+//   }
+// }
 
-module.exports = Product;
+// module.exports = Product;
