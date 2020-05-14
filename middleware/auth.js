05b3e848a7debd817e8993ff8e5ca4be77ba0d29
henriqueeyer <henriqueeyer@outlook.com>
2020-05-14 19:14:25 -0300

    Purchase and products done

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,18 +4,15 @@ const User = require('../models/user');
 const factory = (secret) => async (req, res, next) => {
   const token = req.headers['authorization'];
 
-  if (!token) {
-    return res.status(401).json({ message: 'no auth token provided' });
-  }
+  if (!token) return res.status(401).json({ message: 'no auth token provided' });
+
 
   try {
     const payload = jwt.verify(token, secret);
 
     const user = await User.findOneById(payload.data);
 
-    if (!user) {
-      return res.status(401).json({ message: 'invalid token user' });
-    }
+    if (!user) return res.status(401).json({ message: 'invalid token user' });
 
     req.user = user;
 
