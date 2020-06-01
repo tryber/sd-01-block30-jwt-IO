681bb58c70716cff8201a09a50383a3bd0e2bf53
Douglas Eduardo <douglaseduardo220483@gmail.com>
2020-05-31 22:29:40 -0300

    verificando com começar o app

--- api/server.js
+++ api/server.js
@@ -8,6 +8,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const apiRoutes = express.Router();
+
+apiRoutes.get('/', (req, res) => {
+  return res.status(200).json({ message: 'essa porra tá funfando ou não?'});
+});
+
 apiRoutes.post('/users', routes.createUsers);
 apiRoutes.post('/login', routes.login)
 
