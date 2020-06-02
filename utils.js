99120e7788f1cbc0c2adbbe0df495e0c41d085d9
Gabriel Dalseco dos Santos <gabriel_santos96@outlook.com>
2020-06-01 23:11:33 -0300

    arrumando projeto

--- utils.js
+++ utils.js
@@ -2,7 +2,7 @@ const rescue = fn => async (req, res, next) => {
   try {
     await fn(req, res, next);
   } catch (err) {
-    res.json({ message: err.name, error: err.message });
+    res.status(401).json({ message: err.name, error: err.message });
     next(err.name);
   }
 };
