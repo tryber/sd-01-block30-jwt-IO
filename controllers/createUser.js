0efcc5826ca734175ce4f9ec6ef8cf42301997a8
vrramosm <vf.ramos7@gmail.com>
2020-06-08 15:42:32 -0300

    Finalizado o requisito 1

--- controllers/createUser.js
+++ controllers/createUser.js
@@ -1,11 +1,21 @@
 const express = require('express');
 
+const { v4: uuid4 } = require('uuid');
+
+const { writeFileJson, readFileJson } = require('../fs-functions');
+
 const router = express.Router();
 
-const validCreateUserMiddleware = require('../middlewares/createUser');
+const { validCreateUserMiddleware } = require('../middlewares/createUser');
 
-router.post('/', validCreateUserMiddleware, (_req, res) => {
+router.post('/', validCreateUserMiddleware, async (req, res) => {
+  const newUsersJson = await readFileJson('users');
+  const user = { id: uuid4(), ...req.body };
+  newUsersJson.push(user);
+
+  await writeFileJson(newUsersJson, 'users');
+
   res.status(201).json({ message: 'User successfully registered' });
 });
 
-module.exports = { router };
+module.exports = router;
--- middlewares/createUser.js
+++ middlewares/createUser.js
@@ -1,4 +1,8 @@
-function userNameValidation (username = '') {
+const fs = require('fs');
+
+const path = require('path');
+
+function userNameValidation(username = '') {
   const regex = /[a-zA-Z0-9]+/;
   return username.match(regex) && username.length >= 6;
 };
@@ -12,11 +16,20 @@ function roleValidation(role = '') {
   return roleOptions.includes(role);
 }
 
+function userUniqueValidation (username) {
+  const content = fs.readFileSync(path.resolve(__dirname, '..', 'users.json'), 'utf-8');
+  const uniqueUser = JSON.parse(content);
+
+  return uniqueUser.find(user => user.username === username);
+}
+
 function validCreateUserMiddleware(req, res, next) {
   const { username, password, role } = req.body;
-  if (!userNameValidation(username) || !passwordValidation(password) || !roleValidation(role)) {
+  if (!userNameValidation(username) || !passwordValidation(password) || !roleValidation(role))
     return res.status(400).json({ message: 'Invalid fields' });
-  }
+
+  if (userUniqueValidation(username))
+    return res.status(400).json({ message: 'User must be unique' });
 
   next();
 }
