5a5e3b6cc4dda85b8eeb31d3f46746ebfbbff8eb
Guilherme Luiz <guiiluiz44@gmail.com>
2020-06-08 16:39:19 -0300

    fix codeclimate errors

--- controllers/products/setImage.js
+++ controllers/products/setImage.js
@@ -4,5 +4,5 @@ module.exports = async (req, res) => {
   const productId = req.body.productId;
   const image = req.file;
   if (!productId || !image) return res.status(400).json({ message: 'Parâmetro Inválido!' });
-  return Products.setImage({ productId, image }).then(image => res.status(201).json(image));
+  return Products.setImage({ productId, image }).then(img => res.status(201).json(img));
 };
